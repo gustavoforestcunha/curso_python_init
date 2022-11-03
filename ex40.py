c0bf48426339200bee70be36270c2ff5b83fd752
gustavoforestcunha <115732562+gustavoforestcunha@users.noreply.github.com>
2022-11-03 16:26:31 -0300

    update

--- ex40.py
+++ ex40.py
@@ -1,9 +1,10 @@
 nota1 = float(input('DIGITE A PRIMEIRA NOTA: '))
 nota2 = float(input('DIGITE A SEGUNDA NOTA: '))
-media = nota1 + nota2 / 2
+media = (nota1 + nota2) / 2
 if media < 5:
     print('REPROVADO')
 elif media >= 5 and media < 7:
     print('EM RECUPERAÇÃO')
 else:
     print('APROVADO')
+print('Sua nota é de {}'.format(media))

ab7c66b8fa8b5aab06784bf2871768d444ef24f0
gustavoforestcunha <115732562+gustavoforestcunha@users.noreply.github.com>
2022-11-04 15:45:31 -0300

    update

--- frase.py
+++ frase.py
@@ -1,4 +1,4 @@
 frase = str(input('digite uma frase: ')).strip().upper()
 print('a letra A aparece {} vezes na frase.'.format(frase.count('A')))
-print('a primeira letra A apareceu na poisção {}'.format(frase.find('A')+1))
-print('a ultima letra A apareceu na poisção {}'.format(frase.rfind('A')+1))
+print('a primeira letra A apareceu na poisção {}'.format(frase.find('A') + 1))
+print('a ultima letra A apareceu na poisção {}'.format(frase.rfind('A') + 1))

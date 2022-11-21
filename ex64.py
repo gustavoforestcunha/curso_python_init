b79050b05d5f62a4d8da94fa70ba36eead28dc63
gustavoforestcunha <115732562+gustavoforestcunha@users.noreply.github.com>
2022-11-21 16:47:13 -0300

    update

--- ex64.py
+++ ex64.py
@@ -1,8 +1,7 @@
-count = 0
-n = int(input('Digite um numero: '))
+n = cont = soma = 0
 while n != 999:
-    n = int(input('Digite um numero: '))
-    count = count + 1
-    soma = n + n
-else:
-    print('Programa encerrado. Você digitou {} numeros. e a soma de todos eles foi {}'.format(count, soma))
+    n = int(input('Digite um numero [999 para parar]: '))
+    soma += n
+    cont = cont + 1
+print('Você digitou {} numeros e a soma entre eles foi {}.'.format(
+    cont - 1, soma - 999))

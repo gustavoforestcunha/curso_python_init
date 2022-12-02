a8bca64851af6d89e630ee3682e6b1eb7e0b1d12
gustavoforestcunha <115732562+gustavoforestcunha@users.noreply.github.com>
2022-12-02 00:51:05 -0300

    update

---
+++ ex91.py
@@ -0,0 +1,16 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'Jogador 1': randint(1, 6),
+        'Jogador 2': randint(1, 6),
+        'Jogador 3': randint(1, 6),
+        'Jogador 4': randint(1, 6)}
+ranking = {}
+print('Valores Sorteados: ')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+print('-=' * 30)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}')
